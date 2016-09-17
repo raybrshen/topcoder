60fa7034e7dbb6f819bb26dac236b779389b9651
ray <raybrshen@gmail.com>
2016-09-17 14:38:55 -0400

    Added explanation comments.

--- SRM_144_1_1.py
+++ SRM_144_1_1.py
@@ -1,6 +1,17 @@
 __author__ = 'ray'
 # problem url: https://community.topcoder.com/stat?c=problem_statement&pm=1704
 
+# Encrypt a binary string by adding to each digit the sum of its adjacent digits.
+# If P is the original string, and Q is the encrypted string, then Q[i] = P[i-1] + P[i] + P[i+1] for all digit positions i.
+# Characters off the left and right edges of the string are treated as zeroes.
+# Given an encrypted string, return the possible original string.
+
+# There are two ways to decrypt the string, by assumming either 0 or 1 as the first digit of the original string.
+# Starting from the second digit, digit P[i] can be calculated by P[i]=Q[i-1]-P[i-1]-P[i-2].
+# Propagate in this way until the whole string is calculated, verify that
+#   - each digit in P is either 0 or 1
+#   - the last digit Q[n] equals to P[n-1]+P[n]
+# Return decrypted string if it is valid.
 class BinaryCode:
 	def decode(self, message):
 		ret_0_flag = True
